8ece84f1eff47bc172bc7ae0bed2dffb3d7ddd1f
Gustavo Zavon Pestana <gpestana04@gmail.com>
2026-05-04 13:43:52 -0300

    feat: Test

--- src/test/java/State/PersonagemTest.java
+++ src/test/java/State/PersonagemTest.java
@@ -1,4 +1,35 @@
 package State;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PersonagemTest {
+
+    @Test
+    void deveIniciarNormal() {
+        Personagem p = new Personagem();
+        assertEquals("Normal", p.getNomeEstado());
+    }
+
+    @Test
+    void deveEnvenenar() {
+        Personagem p = new Personagem();
+        p.envenenar();
+        assertEquals("Envenenado", p.getNomeEstado());
+    }
+
+    @Test
+    void deveCurarEnvenenado() {
+        Personagem p = new Personagem();
+        p.envenenar();
+        p.curar();
+        assertEquals("Normal", p.getNomeEstado());
+    }
+
+    @Test
+    void deveMorrer() {
+        Personagem p = new Personagem();
+        p.morrer();
+        assertEquals("Morto", p.getNomeEstado());
+    }
 }
